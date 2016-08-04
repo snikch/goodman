582c0e8fb85aa12ecd017e8ab07fa2ccfbf76df3
Domenic Del Nano <ddelnano@gmail.com>
2016-08-03 22:54:42 -0400

    Add intial implementation of UnmarshalJSON for transaction.Headers

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,6 +1,10 @@
 package transaction
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
 
 // Transaction represents a Dredd transaction object.
 // http://dredd.readthedocs.org/en/latest/hooks/#transaction-object-structure
@@ -11,16 +15,16 @@ type Transaction struct {
 	Protocol string `json:"protocol,omitempty"`
 	FullPath string `json:"fullPath,omitempty"`
 	Request  *struct {
-		Body    string                 `json:"body,omitempty"`
-		Headers map[string]interface{} `json:"headers,omitempty"`
-		URI     string                 `json:"uri,omitempty"`
-		Method  string                 `json:"method,omitempty"`
+		Body    string  `json:"body,omitempty"`
+		Headers Headers `json:"headers,omitempty"`
+		URI     string  `json:"uri,omitempty"`
+		Method  string  `json:"method,omitempty"`
 	} `json:"request,omitempty"`
 	Expected *json.RawMessage `json:"expected,omitempty"`
 	Real     *struct {
-		Body       string                 `json:"body"`
-		Headers    map[string]interface{} `json:"headers"`
-		StatusCode int                    `json:"statusCode"`
+		Body       string  `json:"body"`
+		Headers    Headers `json:"headers"`
+		StatusCode int     `json:"statusCode"`
 	} `json:"real,omitempty"`
 	Origin  *json.RawMessage `json:"origin,omitempty"`
 	Test    *json.RawMessage `json:"test,omitempty"`
@@ -30,6 +34,55 @@ type Transaction struct {
 
 	TestOrder []string `json:"hooks_modifications,omitempty"`
 }
+
+type Headers map[string][]string
+
+func (hdrs *Headers) UnmarshalJSON(data []byte) error {
+	var headers map[string]interface{} = make(map[string]interface{})
+	err := json.Unmarshal(data, &headers)
+
+	if err != nil {
+		return err
+	}
+
+	*hdrs = make(map[string][]string)
+	for k := range headers {
+		header := headers[k]
+		value := reflect.ValueOf(header)
+		kind := value.Kind()
+
+		switch kind {
+		case reflect.Float32, reflect.Float64, reflect.Int, reflect.String:
+			(*hdrs)[k] = []string{
+				stringFromValue(value),
+			}
+		case reflect.Slice:
+			for i := 0; i < value.Len(); i++ {
+				(*hdrs)[k] = append((*hdrs)[k], stringFromValueAtIndex(value, i))
+			}
+
+		default:
+			// TODO: Figure out how to use this
+			panic(fmt.Sprintf("unsupported Kind %s", kind.String()))
+		}
+
+	}
+
+	return nil
+}
+
+func stringFromValueAtIndex(val reflect.Value, index int) string {
+	return fmt.Sprint(val.Index(index).Interface())
+}
+
+func stringFromValue(val reflect.Value) string {
+	return fmt.Sprint(val.Interface())
+}
+
+// func (t *Transaction) UnmarshalJSON(data []byte) error {
+// 	fmt.Printf("%v", string(data))
+// 	return json.Unmarshal(data, Transaction{})
+// }
 
 // AddTestOrderPoint adds a value to the hooks_modification key used when
 // running dredd with TEST_DREDD_HOOKS_HANDLER_ORDER enabled.
--- transaction/transaction_test.go
+++ transaction/transaction_test.go
@@ -1,6 +1,14 @@
 package transaction
 
-import "testing"
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+type HeadersWrapper struct {
+	Headers Headers `json:"headers,omitempty"`
+}
 
 func TestSettingFailToString(t *testing.T) {
 	trans := Transaction{}
@@ -8,5 +16,33 @@ func TestSettingFailToString(t *testing.T) {
 
 	if trans.Fail != "Fail" {
 		t.Errorf("Transaction.Fail member must be able to be set as string")
+	}
+}
+
+func TestHeadersJsonUnmarshal(t *testing.T) {
+	data := []byte(`{
+	    "headers":{
+		"User-Agent":"Dredd/1.4.0 (Darwin 15.4.0; x64)",
+		"Content-Length":11,
+		"Set-Cookie":["Test=Yo"]
+	    }
+	}`)
+
+	hdrs := &HeadersWrapper{}
+	err := json.Unmarshal(data, hdrs)
+	if err != nil {
+		t.Errorf("Error %v", err)
+	}
+
+	if len(hdrs.Headers["Set-Cookie"]) != 1 || !reflect.DeepEqual(hdrs.Headers["Set-Cookie"], []string{"Test=Yo"}) {
+		t.Errorf("Set-Cookie should be slice with length of 1")
+	}
+
+	if !reflect.DeepEqual(hdrs.Headers["User-Agent"], []string{"Dredd/1.4.0 (Darwin 15.4.0; x64)"}) {
+		t.Errorf("User-Agent should be converted to a slice")
+	}
+
+	if !reflect.DeepEqual(hdrs.Headers["Content-Length"], []string{"11"}) {
+		t.Errorf("Content-Length should be converted to a string and be a slice")
 	}
 }
