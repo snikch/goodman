9fceff8e4d370c41cf68d1db8c879113f29c1607
Domenic Del Nano <ddelnano@gmail.com>
2017-02-21 23:42:11 -0500

    Return 500 on error instead of panicing

--- example/main.go
+++ example/main.go
@@ -20,12 +20,15 @@ func main() {
 		db, err := sql.Open("sqlite3", "./foo.db")
 		if err != nil {
 			log.Fatal(err)
+			w.WriteHeader(500)
+			return
 		}
 		defer db.Close()
 		rows, err := db.Query(`select id, name, email from users where id = ?;`, 1)
 
 		if err != nil {
-			panic(err)
+			w.WriteHeader(500)
+			return
 		}
 
 		var id int
@@ -46,7 +49,8 @@ func main() {
 		}
 		data, err := json.Marshal(user)
 		if err != nil {
-			panic("Marshaling failed with message " + err.Error())
+			w.WriteHeader(500)
+			return
 		}
 		w.Header().Add("Content-Type", "application/json")
 		w.Write(data)
